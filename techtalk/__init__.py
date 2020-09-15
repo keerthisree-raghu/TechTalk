9b244b9b8e0ece6d6c29829963e250ab5ac45dfa
Keerthisree <raghu.keerthisree@gmail.com>
2020-09-15 18:05:35 +0530

    Add user authentication and CRUD functionalities for posts

---
+++ techtalk/__init__.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '7abd6ad8149a19bc90057467e24b6ec2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'YOUR_EMAIL_ID'
+app.config['MAIL_PASSWORD'] = 'YOUR_EMAIL_PASSWORD'
+mail = Mail(app)
+
+from techtalk import routes
